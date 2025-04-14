a081f3eb8dd35e672059244deedaa86d65d1836c
Vincent Burckhardt <vincent.burckhardt@ie.ibm.com>
2025-04-14 11:00:07 +0100

    init

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "null_resource" "install_ibmcloud_cli" {
   provisioner "local-exec" {
     interpreter = ["/bin/bash", "-c"]
-    command = "${path.module}/scripts/install_ibmcloud_cli.sh"
+    command = "${path.module}/scripts/install-ibmcloud-cli.sh"
   }
 }
+
